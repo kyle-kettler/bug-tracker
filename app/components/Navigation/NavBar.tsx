c4317cdcafb6863ec8e7984700d0506234d06b5a
Kyle Kettler <hello@kylekettler.com>
2023-11-02 14:53:41 -0500

    styled nav and active link with clsx

--- app/components/Navigation/NavBar.tsx
+++ app/components/Navigation/NavBar.tsx
@@ -1,14 +1,34 @@
+"use client"
+
 import React from "react";
 import Link from "next/link";
-import NavLink from "./NavLink";
 import { Bug } from "lucide-react"
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 export default function NavBar() {
+  const currentPath = usePathname();
+
+  const paths = [
+    {href: "/", label: "Dashboard"},
+    {href: "/issues", label: "Issues"},
+  ]
+
   return (
     <nav className="flex items-center gap-4 border-b mb-6 py-2 px-4 h-14">
       <Link href="/"><Bug size={16}/></Link>
-      <NavLink text="Home" to="/" />
-      <NavLink text="Issues" to="/issues" />
+      <ul className="flex items-center gap-4">
+        {paths.map((link) => (
+        <Link
+          key={link.href}
+          className={clsx(
+            "hover:text-zinc-800 transition-colors",
+            link.href === currentPath && "text-zinc-900",
+            link.href !== currentPath && "text-zinc-500"
+          )}
+          href={link.href}>{link.label}</Link>)
+        )}
+      </ul>
     </nav>
   )
 }
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function IssuesPage() {
+  return (
+    <div>Issues page</div>
+  )
+}
